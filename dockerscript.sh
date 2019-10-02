2c32d8eb34059ac08cd4f56d92de23387e7074a6
kohbah <53706633+kohbah@users.noreply.github.com>
2019-10-02 13:38:16 -0400

    Update dockerscript.sh

--- dockerscript.sh
+++ dockerscript.sh
@@ -4,6 +4,7 @@ cd
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
+cd /
 sudo mkdir /var/jenkins_home
 docker run -d -v /var/jenkins_home:/var/jenkins_home -p 8080:8080 \
 -p 50000:50000 --restart=always --name jenkins jenkins/jenkins:lts \
