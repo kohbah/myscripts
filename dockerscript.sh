e663e85b4f51fb9706c647272b1c1efbf2414ae1
kohbah <53706633+kohbah@users.noreply.github.com>
2019-10-02 14:13:07 -0400

    no restart option

--- dockerscript.sh
+++ dockerscript.sh
@@ -6,6 +6,6 @@ sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 cd /
 sudo mkdir /var/jenkins_home
-sudo docker run -d -v /var/jenkins_home:/var/jenkins_home -p 8080:8080 \
--p 50000:50000 --restart=always --name jenkins jenkins/jenkins:lts \
+sudo docker run -d -ti -v /var/jenkins_home:/var/jenkins_home -p 8080:8080 \
+-p 50000:50000 --name jenkins jenkins/jenkins:lts \
 \
