27f4c5192f196c366aadae0424b5f170c2368baa
kohbah <53706633+kohbah@users.noreply.github.com>
2019-10-13 04:44:44 -0400

    Update jfrog.sh

--- jfrog.sh
+++ jfrog.sh
@@ -7,9 +7,9 @@ sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 sudo yum install java-1.8.0-openjdk-devel
 cd /
-sudo docker pull docker.bintray.io/jfrog/artifactory-oss:latest
+sudo docker pull docker.bintray.io/jfrog/artifactory-pro:latest
 sudo docker volume create --name artifactory_volume
 sudo docker run --name artifactory -d -v \
 artifactory_volume:/var/opt/jfrog/artifactory -p 8081:8081 \
---restart always docker.bintray.io/jfrog/artifactory-oss:latest \
+--restart always docker.bintray.io/jfrog/artifactory-pro:latest \
 \
