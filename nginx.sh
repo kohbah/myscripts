bc6bc0a706f4552adbdf74385f92c88e7e87de5d
kohbah <53706633+kohbah@users.noreply.github.com>
2019-10-17 18:54:32 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,5 @@
+cd /
+sudo mkdir -p /etc/nginx && sudo touch /etc/nginx/default.conf
 sudo docker run --name nginx -d \
 -v /etc/nginx/default.conf:/etc/nginx/default.conf \
 -p 80:80 --restart always nginx:latest \
