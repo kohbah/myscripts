4e5e715708e4bf9a52e7549fdd527611a46edfed
kohbah <53706633+kohbah@users.noreply.github.com>
2019-12-06 11:36:16 -0500

    Create customvimbash.sh

---
+++ customvimbash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/linuxacademy/content-python3-sysadmin/master/helpers/bashrc -o ~/.bashrc
+curl https://raw.githubusercontent.com/linuxacademy/content-python3-sysadmin/master/helpers/vimrc -o ~/.vimrc
